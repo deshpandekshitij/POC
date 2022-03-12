d4a7ee2475070fb165073392fdec7462acced569
deshpandekshitij <kshitijdeshpande1998@gmail.com>
2022-03-12 17:32:47 +0530

    SP call using callable statement

--- src/main/java/com/example/poc/serviceimpl/PocServiceImpl.java
+++ src/main/java/com/example/poc/serviceimpl/PocServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.poc.serviceimpl;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.CallableStatementCreator;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.stereotype.Service;
 
 import com.example.poc.service.PocService;
@@ -18,14 +25,39 @@ public class PocServiceImpl implements PocService{
 
 	@Override
 	public int callStoredProcedure(int inParam) {
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
-				.withProcedureName("sample_sp");
+		List<SqlParameter> parameters = Arrays.asList(new SqlParameter(Types.NVARCHAR), 
+				new SqlParameter(Types.NVARCHAR),
+				new SqlParameter(Types.NVARCHAR),
+				new SqlParameter(Types.NVARCHAR),
+				new SqlParameter(Types.NVARCHAR),
+				new SqlOutParameter("STATUS" ,Types.INTEGER),
+				new SqlOutParameter("USER_ID", Types.INTEGER),
+				new SqlOutParameter("SESSION_ID", Types.NVARCHAR));
+		String userName = "abcd";
+		String ssoKey = "sso parameter";
+		String app = "app parameter";
+		String remoteAddr = "remote address";
 		
-		Map<String, Object> result = simpleJdbcCall.execute(new MapSqlParameterSource("in_param", inParam));
+		Map<String, Object> output = jdbcTemplate.call(new CallableStatementCreator() {
+
+			@Override
+			public CallableStatement createCallableStatement(Connection con) throws SQLException {
+
+				CallableStatement cs = con.prepareCall("{call PR_AUTHENTICATE_AD_ENT(?, ?, ?, ?, ?, ?, ?, ?)}");
+				cs.setString(1, userName);
+				cs.setString(2, ssoKey);
+				cs.setString(3, app);
+				cs.setString(4, "UNIX");
+				cs.setString(5, remoteAddr);
+				cs.registerOutParameter(6, Types.INTEGER);
+				cs.registerOutParameter(7, Types.INTEGER);
+				cs.registerOutParameter(8, Types.NVARCHAR);
+				return cs;
+			}
+		}, parameters);
 		
-		System.out.println(result.get("OUT_PARAM"));
-		
-		return  Integer.parseInt(result.get("OUT_PARAM").toString());
+		System.out.println(output);
+		return Integer.parseInt(output.get("STATUS").toString());
 	}
 
 	
